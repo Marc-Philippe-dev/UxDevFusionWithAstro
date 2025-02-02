1c2d49ee135bc632720854190c332d71d39a58ef
GNANCADJA Marc Philippe <marcgnancadja4@gmail.com>
2025-02-02 09:50:48 +0100

    Shadcn ui configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import icon from "astro-icon"
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
 // https://astro.build/config
+
 export default defineConfig({
-	integrations:[icon()]
+  integrations: [
+    icon(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+
+    react(),
+  ],
+
+  vite: {
+    plugins: [],
+  },
 });
