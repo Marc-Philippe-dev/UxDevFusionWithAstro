5242ee728eeaf27fb054970acf995b508c668ca3
GNANCADJA Marc Philippe <marcgnancadja4@gmail.com>
2025-02-04 14:49:24 +0100

    Footer implementation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,89 +1,83 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ["class"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-  	extend: {
-  		colors: {
-  			primary: {
-  				'50': '#fbf7eb',
-  				'100': '#f5eccc',
-  				'200': '#edd89b',
-  				'300': '#e3bc61',
-  				'400': '#d9a136',
-  				'500': '#cf902a',
-  				'600': '#ad6d21',
-  				'700': '#8b4f1d',
-  				'800': '#74401f',
-  				'900': '#633720',
-  				'950': '#391b0f',
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				'50': '#eef2ff',
-  				'100': '#e0e7ff',
-  				'200': '#c7d1fe',
-  				'300': '#a6b4fb',
-  				'400': '#828cf7',
-  				'500': '#6466f0',
-  				'600': '#5247e4',
-  				'700': '#4539c9',
-  				'800': '#3931a2',
-  				'900': '#332f80',
-  				'950': '#242057',
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		fontFamily: {
-  			josefin: [
-  				'Josefin sans',
-  				'sans-serif'
-  			],
-  			montserrat: [
-  				'Montserrat',
-  				'serif'
-  			]
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      colors: {
+        primary: {
+          50: "#fbf7eb",
+          100: "#f5eccc",
+          200: "#edd89b",
+          300: "#e3bc61",
+          400: "#d9a136",
+          500: "#cf902a",
+          600: "#ad6d21",
+          700: "#8b4f1d",
+          800: "#74401f",
+          900: "#633720",
+          950: "#4D2F00",
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          50: "#eef2ff",
+          100: "#e0e7ff",
+          200: "#c7d1fe",
+          300: "#a6b4fb",
+          400: "#828cf7",
+          500: "#6466f0",
+          600: "#5247e4",
+          700: "#4539c9",
+          800: "#3931a2",
+          900: "#332f80",
+          950: "#242057",
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
+      },
+      fontFamily: {
+        josefin: ["Josefin sans", "sans-serif"],
+        montserrat: ["Montserrat", "serif"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
