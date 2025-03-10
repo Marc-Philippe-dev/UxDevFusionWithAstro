282abcf745a988ba6a6a0753bedd5f6968e7c951
GNANCADJA Marc Philippe <marcgnancadja4@gmail.com>
2025-03-10 11:02:22 +0100

    Contact page implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
+// import adapter from "@astro/adapter-static";
 // https://astro.build/config
 
 export default defineConfig({
+  // output: "static",
+  // adapter: github(),
+  // base: "/UxDevFusionWithAstro/",
   integrations: [
     icon(),
     tailwind({
