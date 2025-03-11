6cb70881d5ac15608e9f1810d982c75fc2289bb1
GNANCADJA Marc Philippe <marcgnancadja4@gmail.com>
2025-03-11 10:51:20 +0100

    Deployement

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import react from "@astrojs/react";
 // https://astro.build/config
 
 export default defineConfig({
-  site: "https://marc-philippe-dev.github.io",
-  base: "/UxDevFusionWithAstro/",
+  // site: "https://marc-philippe-dev.github.io",
+  // base: "/UxDevFusionWithAstro/",
   integrations: [
     icon(),
     tailwind({
